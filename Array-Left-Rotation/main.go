62c5e81afb74c5b98767ee83d007617c790669e7
Bruno Melo <bruno.b.melo@live.com>
2021-10-23 00:35:07 -0300

    add main.go

--- Array-Left-Rotation/main.go
+++ Array-Left-Rotation/main.go
@@ -6,15 +6,5 @@ import (
 )
 
 func main() {
-	var n, d int
-	fmt.Scanln(&n, &d)
-
-	var a = make([]string, n, n)
-	var pos = n - d
-	for i := 0; i < n; i++ {
-		fmt.Scan(&a[pos])
-		pos = (pos + 1) % n
-	}
-
-	fmt.Println(strings.Join(a, " "))
+// :D
 }
